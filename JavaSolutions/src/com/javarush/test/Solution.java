3662ea3d640bde0c64e054af5beae124630d3c33
Alik72 <sadvik19@gmail.com>
2017-01-02 15:31:22 +0600

    print(int) и print(String)
    level12.lesson04.task01;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,41 +3,17 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Или «Корова», или «Кит», или «Собака», или «Неизвестное животное»
-level12.lesson02.task05;
-Написать метод, который определяет, объект какого класса ему передали, и возвращает результат – одно значение из: «Корова», «Кит», «Собака», «Неизвестное животное».
+/* print(int) и print(String)
+level12.lesson04.task01;
+Написать два метода: print(int) и print(String).
 */
 
 public class Solution
 {
   public static void main(String[] args)
   {
-    System.out.println(getObjectType(new Cow()));
-    System.out.println(getObjectType(new Dog()));
-    System.out.println(getObjectType(new Whale()));
-    System.out.println(getObjectType(new Pig()));
+
   }
 
-  public static String getObjectType(Object o)
-  {
-    //Напишите тут ваше решение
-
-    return "Неизвестное животное";
-  }
-
-  public static class Cow
-  {
-  }
-
-  public static class Dog
-  {
-  }
-
-  public static class Whale
-  {
-  }
-
-  public static class Pig
-  {
-  }
+  //Напишите тут ваши методы
 }
