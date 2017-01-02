56deb509b56e6a605feb5060fb981e518f19d2c0
Alik72 <sadvik19@gmail.com>
2017-01-02 15:50:44 +0600

    Пять методов print с разными параметрами
    level12.lesson04.task03;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,10 +3,9 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* print(int) и print(Integer)
-level12.lesson04.task02;
-Написать два метода: print(int) и print(Integer).
-Написать такой код в методе main, чтобы вызвались они оба.
+/* Пять методов print с разными параметрами
+level12.lesson04.task03;
+Написать пять методов print с разными параметрами.
 */
 
 public class Solution
