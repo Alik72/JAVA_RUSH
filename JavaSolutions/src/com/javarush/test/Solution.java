486a32f21b76c869c00cdb8f67a8ffc061f937c3
Alik72 <sadvik19@gmail.com>
2017-01-02 15:55:43 +0600

    Три метода возвращают минимальное из двух переданных в него чисел
    level 12.lesson04.task04;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,9 +3,10 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Пять методов print с разными параметрами
-level12.lesson04.task03;
-Написать пять методов print с разными параметрами.
+/* Три метода возвращают минимальное из двух переданных в него чисел
+level 12.lesson04.task04;
+Написать public static методы: int min(int, int), long min(long, long), double min(double, double).
+Каждый метод должен возвращать минимальное из двух переданных в него чисел.
 */
 
 public class Solution
