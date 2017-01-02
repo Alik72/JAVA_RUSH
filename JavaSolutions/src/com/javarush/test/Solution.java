31e6f9e140757e0bb6fccf601975a9ecbb85ae6c
Alik72 <sadvik19@gmail.com>
2017-01-02 16:01:32 +0600

    Три метода возвращают максимальное из двух переданных в него чисел
    level 12.lesson04.task05;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,10 +3,10 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Три метода возвращают минимальное из двух переданных в него чисел
-level 12.lesson04.task04;
-Написать public static методы: int min(int, int), long min(long, long), double min(double, double).
-Каждый метод должен возвращать минимальное из двух переданных в него чисел.
+/* Три метода возвращают максимальное из двух переданных в него чисел
+level 12.lesson04.task05;
+Написать public static методы: int max(int, int), long max (long, long), double max (double, double).
+Каждый метод должен возвращать максимальное из двух переданных в него чисел.
 */
 
 public class Solution
