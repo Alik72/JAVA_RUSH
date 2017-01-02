7b9784e4ed89da20b61f03b1c7d5dd3cf2d46ab7
Alik72 <sadvik19@gmail.com>
2017-01-02 15:04:46 +0600

    Или «Корова», или «Кит», или «Собака», или «Неизвестное животное»
    level12.lesson02.task05;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,28 +3,29 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Или «Кошка», или «Собака», или «Птица», или «Лампа»
-level12.lesson02.task04;
-Написать метод, который определяет, объект какого класса ему передали, и выводит на экран одну из надписей: Кошка, Собака, Птица, Лампа.
+/* Или «Корова», или «Кит», или «Собака», или «Неизвестное животное»
+level12.lesson02.task05;
+Написать метод, который определяет, объект какого класса ему передали, и возвращает результат – одно значение из: «Корова», «Кит», «Собака», «Неизвестное животное».
 */
 
 public class Solution
 {
   public static void main(String[] args)
   {
-    printObjectType(new Cat());
-    printObjectType(new Bird());
-    printObjectType(new Lamp());
-    printObjectType(new Cat());
-    printObjectType(new Dog());
+    System.out.println(getObjectType(new Cow()));
+    System.out.println(getObjectType(new Dog()));
+    System.out.println(getObjectType(new Whale()));
+    System.out.println(getObjectType(new Pig()));
   }
 
-  public static void printObjectType(Object o)
+  public static String getObjectType(Object o)
   {
     //Напишите тут ваше решение
+
+    return "Неизвестное животное";
   }
 
-  public static class Cat
+  public static class Cow
   {
   }
 
@@ -32,11 +33,11 @@ public class Solution
   {
   }
 
-  public static class Bird
+  public static class Whale
   {
   }
 
-  public static class Lamp
+  public static class Pig
   {
   }
 }
