a88cf8184a978bbb93119d4560e6e9a536bc2f06
Alik72 <sadvik19@gmail.com>
2017-01-04 10:53:08 +0600

    Задача по алгоритмам
    level 12.lesson12.bonus03;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,25 +3,45 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Метод, который выводит на экран число 10
-Добавь еще один метод, чтобы программа выводила на экран число 10.
-Подсказка: используй перегрузку методов.
+/* Задача по алгоритмам
+level12.lesson12.bonus03;
+Написать метод, который возвращает минимальное число в массиве и его позицию (индекс).
 */
 
 public class Solution
 {
-  public static void main(String[] args)
+  public static void main(String[] args) throws Exception
   {
-    Integer i = 5;
-    int x = transformValue(i);
+    int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
 
-    System.out.println(x);
+    Pair<Integer, Integer> result = getMinimumAndIndex(data);
+
+    System.out.println("Minimum is " + result.x);
+    System.out.println("Index of minimum element is " + result.y);
   }
 
-  public static int transformValue(int i)
+  public static Pair<Integer, Integer> getMinimumAndIndex(int[] array)
   {
-    return i*i;
+    if (array == null || array.length == 0)
+    {
+      return new Pair<Integer, Integer>(null, null);
+    }
+
+
+
+    return new Pair<Integer, Integer>(0, 0);
   }
 
 
+  public static class Pair<X, Y>
+  {
+    public X x;
+    public Y y;
+
+    public Pair(X x, Y y)
+    {
+      this.x = x;
+      this.y = y;
+    }
+  }
 }
