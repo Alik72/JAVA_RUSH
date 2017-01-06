a023cddcaa21b46aceeb426f570af4a1ae16eed2
Alik72 <sadvik19@gmail.com>
2017-01-06 12:19:51 +0600

    4 ошибки
    level 13.lesson02.task05;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,28 +3,43 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Selectable и Updatable
-level 13.lesson02.task04;
-1. Создай класс Screen.
-2. Реализуй в нем интерфейсы Selectable с методом onSelect, Updatable с методом refresh.
+/* 4 ошибки
+level 13.lesson02.task05;
+Исправь 4 ошибки в программе, чтобы она компилировалась.
+Объявление интерфейсов не изменять.
 */
 
 public class Solution
 {
+
   public static void main(String[] args) throws Exception
   {
+
+    System.out.println(new Dream().HOBBIE.toString());
+    System.out.println(new Hobbie().toString());
+
   }
 
-  interface Selectable
+  interface Desire
   {
-    void onSelect();
   }
 
-  interface Updatable
+  interface Dream
   {
-    void refresh();
+    private static Hobbie HOBBIE = new Hobbie();
   }
 
-  //напишите тут ваш класс
+  class Hobbie extends Desire implements Dream
+  {
+    static int INDEX = 1;
+
+    @Override
+    public String toString()
+    {
+      INDEX++;
+      return "" + INDEX;
+    }
+  }
+
 }
 
