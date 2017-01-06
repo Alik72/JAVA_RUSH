3ca08a52941463709f69faef44ce312b0c9dda9a
Alik72 <sadvik19@gmail.com>
2017-01-06 11:19:32 +0600

    Пиво: возвращение
    level 13.lesson02.task02;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,38 +3,37 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Пиво
-level13.lesson02.task01;
-1. Подумай, какой из двух интерфейсов нужно реализовать в классе Beer.
-2. Добавь к классу Beer этот интерфейс и реализуйте все нереализованные методы.
-3. Подумай, как связаны переменная  READY_TO_GO_HOME и метод isReadyToGoHome.
-4. Верни значение переменной READY_TO_GO_HOME в методе isReadyToGoHome.
+/* Пиво: возвращение
+level 13.lesson02.task02;
+Добавь к классу AlcoholicBeer интерфейс Drink и реализуй все нереализованные методы.
 */
 
 public class Solution
 {
   public static void main(String[] args) throws Exception
   {
+    Drink beer = new AlcoholicBeer();
+    System.out.println(beer.toString());
   }
 
   public interface Drink
   {
-    void askMore(String message);
-
-    void sayThankYou();
-
-    boolean isReadyToGoHome();
+    boolean isAlcoholic();
   }
 
-  public interface Alcohol extends Drink
+  public static class AlcoholicBeer
   {
-    boolean READY_TO_GO_HOME = false;
+    @Override
+    public String toString()
+    {
+      if (isAlcoholic()) {
+        return "Напиток алкогольный";
+      }
+      else {
+        return "Напиток безалкогольный";
+      }
 
-    void sleepOnTheFloor();
-  }
-
-  public static class Beer
-  {
+    }
 
   }
 }
