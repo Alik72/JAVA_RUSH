16519feaecd545e3e7d88314174f2264a79f0ef2
Alik72 <sadvik19@gmail.com>
2017-01-06 18:18:20 +0600

    Жив или нет
    level 13.lesson02.task08;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,23 +3,18 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Параметризованый интерфейс
-level 13.lesson02.task07;
-В классе StringObject реализуй интерфейс SimpleObject с параметром типа String.
+/* Жив или нет
+level 13.lesson02.task08;
+1. Создай интерфейс Person.
+2. Добавь в него метод isAlive(), который проверяет, жив человек или нет.
+3. Подумай, какой тип должен возвращать этот метод.
+4. Создай интерфейс Presidentable.
+5. Унаследуй интерфейс Presidentable от интерфейса Person.
 */
 
 public class Solution
 {
   public static void main(String[] args) throws Exception
-  {
-  }
-
-  interface SimpleObject<T>
-  {
-    SimpleObject<T> getInstance();
-  }
-
-  class StringObject //допишите здесь ваш код
   {
   }
 }
