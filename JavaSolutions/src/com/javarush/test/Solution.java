2726de9ea85c070fb24826c99174cbb724a6856d
Alik72 <sadvik19@gmail.com>
2017-01-06 18:35:27 +0600

    Наследование интерфейса
    level13.lesson02.task09;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,13 +3,13 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Жив или нет
-level 13.lesson02.task08;
-1. Создай интерфейс Person.
-2. Добавь в него метод isAlive(), который проверяет, жив человек или нет.
-3. Подумай, какой тип должен возвращать этот метод.
-4. Создай интерфейс Presidentable.
-5. Унаследуй интерфейс Presidentable от интерфейса Person.
+/* Наследование интерфейса
+level13.lesson02.task09;
+1. Создать интерфейс Movable с методом speed.
+2. Метод speed возвращает значение типа Double и не принимает параметров.
+3. Создать и унаследовать интерфейс Runable от интерфейса Movable.
+4. Добавить в интерфейс Runable метод speed.
+5. Метод speed возвращает значение типа Double и принимает один параметр типа Runable.
 */
 
 public class Solution
