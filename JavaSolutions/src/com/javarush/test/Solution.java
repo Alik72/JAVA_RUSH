c9bceaaba57d08cb864bb4be97fd27a0298576fd
Alik72 <sadvik19@gmail.com>
2017-01-06 12:39:43 +0600

    Баг в initializeIdAndName
    level 13.lesson02.task06;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,43 +3,47 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* 4 ошибки
-level 13.lesson02.task05;
-Исправь 4 ошибки в программе, чтобы она компилировалась.
-Объявление интерфейсов не изменять.
+/* Баг в initializeIdAndName
+level 13.lesson02.task06;
+1. Подумать, что в программе неправильно.
+2. Вынести реализацию метода initializeIdAndName в класс User.
+3. initializeIdAndName в классе User должен возвращать тип User.
+4. Поправить программу, чтобы компилировалась и работала.
 */
 
 public class Solution
 {
-
   public static void main(String[] args) throws Exception
   {
-
-    System.out.println(new Dream().HOBBIE.toString());
-    System.out.println(new Hobbie().toString());
-
+    System.out.println(Matrix.NEO);
+    System.out.println(Matrix.TRINITY);
   }
 
-  interface Desire
+  static class Matrix
   {
+    public static DBObject NEO = new User().initializeIdAndName(1, "Neo");
+    public static DBObject TRINITY = new User().initializeIdAndName(2, "Trinity");
   }
 
-  interface Dream
+  interface DBObject
   {
-    private static Hobbie HOBBIE = new Hobbie();
+    DBObject initializeIdAndName(long id, String name) {
+      this.id = id;
+      this.name = name;
+      return this;
+    }
   }
 
-  class Hobbie extends Desire implements Dream
+  static class User implements DBObject
   {
-    static int INDEX = 1;
+    long id;
+    String name;
 
     @Override
     public String toString()
     {
-      INDEX++;
-      return "" + INDEX;
+      return String.format("User has name %s, id = %d", name, id);
     }
   }
 
 }
-
