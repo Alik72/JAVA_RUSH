2b77ecd68026a72c6fa9ac72fb45c37d7c25d8fd
Alik72 <sadvik19@gmail.com>
2017-01-06 18:53:52 +0600

    level13.lesson02.task10;
    CleverMan и SmartGirl

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,18 +3,63 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Наследование интерфейса
-level13.lesson02.task09;
-1. Создать интерфейс Movable с методом speed.
-2. Метод speed возвращает значение типа Double и не принимает параметров.
-3. Создать и унаследовать интерфейс Runable от интерфейса Movable.
-4. Добавить в интерфейс Runable метод speed.
-5. Метод speed возвращает значение типа Double и принимает один параметр типа Runable.
+/* CleverMan и SmartGirl
+level13.lesson02.task10;
+1. Добавь интерфейсы Secretary и Boss к классам CleverMan и SmartGirl. По одному на каждый. Подумай, кому какой.
+2. Унаследуй  интерфейсы Secretary и Boss от интерфейсов Person и Workable так,
+чтобы все методы у классов CleverMan и SmartGirl оказались объявленными в каком-то интерфейсе.
 */
 
 public class Solution
 {
   public static void main(String[] args) throws Exception
   {
+  }
+
+  interface Person
+  {
+    void use(Person person);
+    void startToWork();
+  }
+
+  interface Workable
+  {
+    boolean wantGetExtraWork();
+  }
+
+  interface Secretary
+  {
+  }
+
+  interface Boss
+  {
+  }
+
+  class CleverMan
+  {
+    public void use(Person person)
+    {
+      person.startToWork();
+    }
+
+    public void startToWork()
+    {
+    }
+
+    public boolean wantGetExtraWork()
+    {
+      return true;
+    }
+  }
+
+  class SmartGirl
+  {
+    public void use(Person person)
+    {
+    }
+
+    public void startToWork()
+    {
+    }
   }
 }
