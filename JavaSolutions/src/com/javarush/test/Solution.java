b3c36e6c72e8a05bbb7c71937a00003b3925b509
Alik72 <sadvik19@gmail.com>
2017-01-09 15:15:21 +0600

    Исправление ошибок
    level13.lesson11.home06;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,41 +3,39 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Neo
-level13.lesson11.home05;
-1. Реализовать интерфейс DBObject в классе User.
-2. Реализовать метод initializeIdAndName так, чтобы программа работала и выводила на экран "User has name Neo, id = 1".
-3. Метод toString и метод main менять нельзя.
-4. Подумай, что должен возвращать метод initializeIdAndName в классе User.
+/* Исправление ошибок
+level13.lesson11.home06;
+1. Переделай наследование в классах и интерфейсах так, чтобы программа компилировалась и продолжала делать то же самое.
+2. Класс Hobbie должен наследоваться от интерфейсов Desire, Dream.
 */
 
 public class Solution
 {
+
   public static void main(String[] args) throws Exception
   {
-    System.out.println(Matrix.NEO);
+    System.out.println(Dream.HOBBIE.toString());
+    System.out.println(new Hobbie().INDEX);
   }
 
-
-  static class Matrix
+  interface Desire
   {
-    public static DBObject NEO = new User().initializeIdAndName(1, "Neo");
   }
 
-  interface DBObject
+  interface Dream implements Hobbie
   {
-    DBObject initializeIdAndName(long id, String name);
+    static Hobbie HOBBIE = new Hobbie();
   }
 
-  static class User
+  static class Hobbie extends Desire, Dream
   {
-    long id;
-    String name;
+    static int INDEX = 1;
 
     @Override
     public String toString()
     {
-      return String.format("User has name %s, id = %d", name, id);
+      INDEX++;
+      return "" + INDEX;
     }
   }
 
