28bad4a8c4f59efbc50c493557d2d7d52bd1fd2d
Alik72 <sadvik19@gmail.com>
2017-01-09 16:03:59 +0600

    Компиляция программы
    level 13.lesson11.home10

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -5,33 +5,37 @@ import java.awt.*;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Один метод в классе
-level13.lesson11.home09;
-1. Унаследовать Fox от интерфейса Animal.
-2. Поменяй код так, чтобы в классе Fox был только один метод - getName.
-3. Методы удалять нельзя!
+/* Компиляция программы
+level 13.lesson11.home10;
+1. Исправь классы Fox и BigFox так, чтобы программа компилировалась. Задача не предполагает создания экземпляров базового класса.
+2. Метод main менять нельзя.
 */
 
 public class Solution
 {
+
   public static void main(String[] args) throws Exception
   {
+    Fox bigFox = new BigFox();
+    System.out.println(bigFox.getName());
+    System.out.println(bigFox.getColor());
   }
 
   public interface Animal
   {
     Color getColor();
-
-    Integer getAge();
   }
 
-  public static class Fox
+  public static class Fox implements Animal
   {
-    public String getName()
-    {
+    public String getName() {
       return "Fox";
     }
   }
+
+  public abstract static class BigFox
+  {
+
+  }
+
 }
-
-
