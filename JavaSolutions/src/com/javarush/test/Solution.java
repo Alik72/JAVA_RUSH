e39d84c775f9b3794633c74187cf0d9881fd9979
Alik72 <sadvik19@gmail.com>
2017-01-09 15:54:01 +0600

    Интерфейс Updatable в классе Screen
    level 13.lesson11.home08;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,23 +3,30 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Интерфейс SimpleObject
-level13.lesson11.home07;
-1. Создай класс StringObject.
-2. В классе StringObject реализуй интерфейс SimpleObject с параметром типа String.
-3. Программа должна компилироваться.
+/* Интерфейс Updatable в классе Screen
+level13.lesson11.home08;
+Реализовать интерфейс Updatable в классе Screen.
 */
 
 public class Solution
 {
   public static void main(String[] args) throws Exception
   {
-    SimpleObject<String> stringObject = new StringObject<Object>();
   }
 
-  interface SimpleObject<T>
+  interface Selectable
   {
-    SimpleObject<T> getInstance();
+    void onSelect();
   }
 
+  interface Updatable extends Selectable
+  {
+    void refresh();
+  }
+
+  class Screen
+  {
+
+  }
 }
+
