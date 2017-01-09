94f00e7f911a9994d88a5c16bb2e05194c2ad785
Alik72 <sadvik19@gmail.com>
2017-01-09 15:50:31 +0600

    Интерфейс SimpleObject
    level13.lesson11.home07;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,40 +3,23 @@ package com.javarush.test;
 /**
  * Created by Homosapiens on 19.10.2016.
  */
-/* Исправление ошибок
-level13.lesson11.home06;
-1. Переделай наследование в классах и интерфейсах так, чтобы программа компилировалась и продолжала делать то же самое.
-2. Класс Hobbie должен наследоваться от интерфейсов Desire, Dream.
+/* Интерфейс SimpleObject
+level13.lesson11.home07;
+1. Создай класс StringObject.
+2. В классе StringObject реализуй интерфейс SimpleObject с параметром типа String.
+3. Программа должна компилироваться.
 */
 
 public class Solution
 {
-
   public static void main(String[] args) throws Exception
   {
-    System.out.println(Dream.HOBBIE.toString());
-    System.out.println(new Hobbie().INDEX);
+    SimpleObject<String> stringObject = new StringObject<Object>();
   }
 
-  interface Desire
+  interface SimpleObject<T>
   {
-  }
-
-  interface Dream implements Hobbie
-  {
-    static Hobbie HOBBIE = new Hobbie();
-  }
-
-  static class Hobbie extends Desire, Dream
-  {
-    static int INDEX = 1;
-
-    @Override
-    public String toString()
-    {
-      INDEX++;
-      return "" + INDEX;
-    }
+    SimpleObject<T> getInstance();
   }
 
 }
