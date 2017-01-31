dc0f1c5e465965425d4ce90ebce0f30533754013
Alik72 <sadvik19@gmail.com>
2017-01-31 13:22:49 +0600

    Коты
    level 14.lesson04.task02;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -2,52 +2,89 @@ package com.javarush.test;
 
 import java.awt.*;
 
-/* Building и School
-level 14.lesson04.task01;
-1. Расставь правильно наследование между Building(здание) и School(здание школы).
-2. Подумай, объект какого класса должны возвращать методы getSchool и getBuilding.
-3. Измени null на объект класса Building или School.
+/* Коты
+level 14.lesson04.task02;
+1. Считывать строки(параметры) с консоли, пока пользователь не введет пустую строку(Enter).
+2. Каждый параметр соответствует имени кота.
+Для каждого параметра:
+3. Создать объект cat класса Cat, который равен коту из getCatByKey(String параметр).
+4. Вывести на экран cat.toString().
 */
 
 public class Solution
 {
-  public static void main(String[] args)
+  public static void main(String[] args) throws Exception
   {
-    Building school = getSchool();
-    Building shop = getBuilding();
-
-    System.out.println(school);
-    System.out.println(shop);
+    //Add your code here
   }
 
-  public static Building getSchool()
+  static class CatFactory
   {
-    //измените null на объект класса Building или School
-    return null;
-  }
 
-  public static Building getBuilding()
-  {
-    //измените null на объект класса Building или School
-    return null;
-  }
-
-  static class School /*Add your code here*/
-  {
-    @Override
-    public String toString()
+    static Cat getCatByKey(String key)
     {
-      return "School";
+      Cat cat = null;
+      if ("vaska".equals(key))
+      {
+        cat = new MaleCat("Василий");
+      } else if ("murka".equals(key))
+      {
+        cat = new FemaleCat("Мурочка");
+      } else if ("kiska".equals(key))
+      {
+        cat = new FemaleCat("Кисюлька");
+      } else
+      {
+        cat = new Cat(key);
+      }
+
+      return cat;
     }
   }
 
-  static class Building /*Add your code here*/
+  static class Cat
   {
-    @Override
+    private String name;
+
+    protected Cat(String name)
+    {
+      this.name = name;
+    }
+
+    public String getName()
+    {
+      return this.name;
+    }
+
     public String toString()
     {
-      return "Building";
+      return "Я уличный кот " + getName();
+    }
+  }
+
+  static class MaleCat extends Cat
+  {
+    MaleCat(String name)
+    {
+      super(name);
+    }
+
+    public String toString()
+    {
+      return "Я - солидный кошак по имени " + getName();
+    }
+  }
+
+  static class FemaleCat extends Cat
+  {
+    FemaleCat(String name)
+    {
+      super(name);
+    }
+
+    public String toString()
+    {
+      return "Я - милая кошечка по имени " + getName();
     }
   }
 }
-
