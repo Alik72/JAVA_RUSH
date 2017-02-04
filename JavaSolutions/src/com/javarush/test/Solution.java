f7d09f3053d587adba5706a2d27f7af3ccfd749e
Alik72 <sadvik19@gmail.com>
2017-02-04 13:18:10 +0600

    Урок 13. дополнение - Битва роботов
    level 13.lesson11.bonus03;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -3,26 +3,44 @@ package com.javarush.test;
 import java.util.ArrayList;
 import java.util.List;
 
-/* Репка
-level 13.lesson11.bonus02;
-Сказка Репка:
-1. Реализовать интерфейс RepkaItem в классе Person.
-2. В классе Person реализовать метод pull(Person person), который выводит фразу типа '<name> за <person>'. Пример:
-Бабка за Дедку
-Дедка за Репку
-3. Исправить логическую ошибку цикла в методе tell класса RepkaStory.
-4. Выполнить метод main и наслаждаться сказкой!
+/* Битва роботов
+level 13.lesson11.bonus03;
+1 Разобраться в том, что тут написано.
+2 http://info.javarush.ru/uploads/images/00/00/07/2014/08/12/50f3e37f94.png
+3 Смириться со своей участью и продолжить разбираться в коде.
+4 ...
+5 Порадоваться, что мы все поняли.
+
+6 Изменить код согласно новой архитектуре и добавить новую логику:
+6.1 Сделать класс AbstractRobot абстрактным, вынести логику атаки и защиты из Robot в AbstractRobot.
+6.2 Отредактировать класс Robot учитывая AbstractRobot.
+6.3 Расширить класс BodyPart новой частью тела "грудь".
+6.4 Добавить новую часть тела в реализацию интерфейсов Attackable и Defensable (в классе AbstractRobot).
+
+7 http://info.javarush.ru/uploads/images/00/00/07/2014/08/12/3b9c65580b.png
 */
 
 public class Solution
 {
   public static void main(String[] args)
   {
-    List<Person> plot = new ArrayList<Person>();
-    plot.add(new Person("Репка", "Репку"));
-    plot.add(new Person("Дедка", "Дедку"));
-    plot.add(new Person("Бабка", "Бабку"));
-    plot.add(new Person("Внучка", "Внучку"));
-    RepkaStory.tell(plot);
+    Robot amigo = new Robot("Амиго");
+    Robot enemy = new Robot("Сгибальщик-02");
+
+    doMove(amigo, enemy);
+    doMove(amigo, enemy);
+    doMove(enemy, amigo);
+    doMove(amigo, enemy);
+    doMove(enemy, amigo);
+    doMove(amigo, enemy);
+    doMove(enemy, amigo);
+    doMove(amigo, enemy);
+  }
+
+  public static void doMove(AbstractRobot robotFirst, AbstractRobot robotSecond) {
+    BodyPart attacked = robotFirst.attack();
+    BodyPart defenced = robotFirst.defense();
+    System.out.println(String.format("%s атаковал робота %s, атакована %s, защищена %s",
+            robotFirst.getName(), robotSecond.getName(), attacked, defenced));
   }
 }
