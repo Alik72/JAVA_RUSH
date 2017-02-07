f051351f316dd8088ea136efbb7c1d5e6c64e150
Alik72 <sadvik19@gmail.com>
2017-02-07 15:47:56 +0600

    НОД
    level14.lesson08.bonus02;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -1,36 +1,14 @@
 package com.javarush.test;
-/* Нашествие эксепшенов
-level 14.lesson08.bonus01;
-Заполни массив exceptions 10 различными эксепшенами.
-Первое исключение уже реализовано в методе initExceptions.
+/* НОД
+level14.lesson08.bonus02;
+Наибольший общий делитель (НОД).
+Ввести с клавиатуры 2 целых положительных числа.
+Вывести в консоль наибольший общий делитель.
 */
 
 public class Solution
 {
-  public static List<Exception> exceptions = new ArrayList<Exception>();
-
-  public static void main(String[] args)
+  public static void main(String[] args) throws Exception
   {
-    initExceptions();
-
-    for (Exception exception : exceptions)
-    {
-      System.out.println(exception);
-    }
-  }
-
-  private static void initExceptions()
-  {   //it's first exception
-    try
-    {
-      float i = 1 / 0;
-
-    } catch (Exception e)
-    {
-      exceptions.add(e);
-    }
-
-    //Add your code here
-
   }
 }
