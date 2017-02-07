5e7e5eb85b6a88ab93297e0a756335c287ac1013
Alik72 <sadvik19@gmail.com>
2017-02-07 15:35:48 +0600

    MovieFactory
    level 14.lesson08.home06;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -1,40 +1,63 @@
 package com.javarush.test;
 
-/* Computer
-level14.lesson08.home05;
-1. Создай интерфейс CompItem.
-2. Добавь в него метод String getName().
-3. Создай классы Keyboard, Mouse, Monitor, которые реализуют интерфейс CompItem.
-4. Метод getName() должен возвращать имя класса, например, для класса Keyboard будет "Keyboard".
-5. Создай класс Computer.
-6. В класс Computer добавь приватное поле типа Keyboard.
-7. В класс Computer добавь приватное поле типа Mouse.
-8. В класс Computer добавь приватное поле типа Monitor.
-9. Создай конструктор в классе Computer используя комбинацию клавиш Alt+Insert внутри класса (команда Constructor).
-10 Внутри конструктора инициализируйте все три поля (переменных) класса
-11. Создай геттеры для полей класса Computer (в классе используй комбинацию клавиш Alt+Insert и выбери команду Getter).
-12. Все созданные классы и интерфейс должны быть в отдельных файлах.
-13. Класс Solution менять нельзя.
+/* MovieFactory
+level 14.lesson08.home06;
+Расширение функционала по аналогии, чтение с консоли:
+1. Разобраться, что программа умеет делать.
+2. Все классы должны быть внутри класса Solution.
+3. Добавить классы Cartoon, Thriller.
+4. Разобраться, как мы получаем объект класса SoapOpera по ключу "soapOpera".
+Аналогично получению объекта SoapOpera сделать:
+5. Добавить в MovieFactory.getMovie получение объекта Cartoon для ключа "cartoon".
+6. Добавить в MovieFactory.getMovie получение объекта Thriller для ключа "thriller".
+
+7. Считать с консоли несколько ключей (строк).
+7.1. Ввод заканчивается, как только вводится строка не совпадающая с одной из: "cartoon", "thriller", "soapOpera".
+8. Создать переменную movie класса Movie и для каждой введенной строки(ключа):
+8.1. Получить объект используя MovieFactory.getMovie и присвоить его переменной movie.
+8.2. Вывести на экран movie.getClass().getSimpleName().
 */
 
 public class Solution
 {
-
-  public static void main(String[] args)
+  public static void main(String[] args) throws Exception
   {
-    Computer computer = new Computer();
-    if (isWork(computer.getKeyboard()) &&
-            isWork(computer.getMonitor()) &&
-            isWork(computer.getMouse()))
+    //ввести с консоли несколько ключей (строк), пункт 7
+
+        /*
+8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
+8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
+8.2 вывести на экран movie.getClass().getSimpleName()
+        */
+
+  }
+
+  static class MovieFactory
+  {
+
+    static Movie getMovie(String key)
     {
-      System.out.println("Work!");
+      Movie movie = null;
+
+      //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
+      if ("soapOpera".equals(key))
+      {
+        movie = new SoapOpera();
+      }
+
+      //напишите тут ваш код, пункты 5,6
+
+      return movie;
     }
   }
 
-  public static boolean isWork(CompItem item)
+  static abstract class Movie
   {
-    System.out.println(item.getName());
-    return item.getName() != null && item.getName().length() > 4;
   }
 
+  static class SoapOpera extends Movie
+  {
+  }
+
+  //Напишите тут ваши классы, пункт 3
 }
