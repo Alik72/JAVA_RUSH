903d7e0a9acbd292dd9643eb2c0d3ac3aa6d3867
Alik72 <sadvik19@gmail.com>
2017-02-07 16:08:43 +0600

    ООП - наследование животных
    level 15.lesson02.task02;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -1,23 +1,27 @@
 package com.javarush.test;
-/* ООП - Расставить интерфейсы
-level 15.lesson02.task01;
-1. Добавить все возможные интерфейсы из Movable, Sellable, Discountable в класс Clothes.
-2. Реализовать их методы.
+/* ООП - наследование животных
+level 15.lesson02.task02;
+1. Создать public static класс Goose(Гусь).
+2. Создать public static класс Dragon(Дракон).
+3. Унаследовать класс Goose от BigAnimal или SmallAnimal, подумать, какой логически больше подходит.
+4. Унаследовать класс Dragon от BigAnimal или SmallAnimal, подумать, какой логически больше подходит.
+5. В классах Goose и Dragon переопределить метод String getSize(), расширить видимость до максимальной.
+6. В классе Goose метод getSize должен возвращать строку "Гусь маленький, " + [getSize родительского класса].
+7. В классе Dragon метод getSize должен возвращать строку "Дракон большой, " + [getSize родительского класса].
 */
 
 public class Solution {
-  public static interface Movable {
-    boolean getAllowedAction(String name);
+  //добавьте классы Goose и Dragon тут
+
+  public static class BigAnimal {
+    protected String getSize() {
+      return "как динозавр";
+    }
   }
 
-  public static interface Sellable {
-    Object getAllowedAction(String name);
-  }
-
-  public static interface Discountable {
-    Object getAllowedAction();
-  }
-
-  public static class Clothes {
+  public static class SmallAnimal {
+    String getSize() {
+      return "как кошка";
+    }
   }
 }
