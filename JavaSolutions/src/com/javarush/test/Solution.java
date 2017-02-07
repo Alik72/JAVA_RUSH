90848ee6eefb10d62036057656de9115940f9a3d
Alik72 <sadvik19@gmail.com>
2017-02-07 16:22:17 +0600

    Максимально простой код 3
    level 15.lesson06.task03;

--- JavaSolutions/src/com/javarush/test/Solution.java
+++ JavaSolutions/src/com/javarush/test/Solution.java
@@ -1,42 +1,29 @@
 package com.javarush.test;
-/* Максимально простой код 2
-level 15.lesson06.task02;
-Посмотри, что, связанное с конструкторами, может быть добавлено при компиляции, но в этой программе уже присутствует.
-Упрости код - удали все конструкторы и вызовы конструкторов супер-классов, которые создаются и добавляются автоматически.
-
-PS: Взаимосвязь между объектами классов NakedCat и NormalCat, SiamCat - Is-a (наследование): http://en.wikipedia.org/wiki/Is-a
-http://cs7002.vk.me/c7007/v7007577/1411a/_dAiEola310.jpg
+/* Максимально простой код 3
+level 15.lesson06.task03;
+Упрости код:
+- убери всё то, что будет генерироваться автоматически при компиляции
+- убери все наследования классов, которые и так будут добавлены автоматически при компиляции
+- убери все конструкторы, которые создаются и добавляются автоматически.
 */
 
 public class Solution {
   public static void main(String[] args) {
-    SiamCat simka = new SiamCat("Simka");
-    NakedCat nakedSimka = simka.shave();
   }
 
-  public static class NakedCat {
-    public NakedCat() {
+  public interface Runnable {
+
+  }
+
+  public class Machine extends Object implements Runnable {
+    public Machine() {
       super();
     }
   }
 
-  public static class NormalCat extends NakedCat {
-    public NormalCat() {
+  public class Car extends Machine implements Runnable {
+    public Car() {
       super();
-    }
-
-    public NormalCat(String name) {
-      System.out.println("My name is " + name);
-    }
-
-    public NakedCat shave() {
-      return this;
-    }
-  }
-
-  public static class SiamCat extends NormalCat {
-    public SiamCat(String name) {
-      super(name);
     }
   }
 }
