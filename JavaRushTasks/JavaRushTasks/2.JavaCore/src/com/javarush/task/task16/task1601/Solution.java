5e765941d6669c002e0af75cad21309a737a20d0
Alik72 <sadvik19@gmail.com>
2017-02-16 12:18:35 +0600

    УРОК 16.        My first thread
    task16.task1601;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task16/task1601/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task16.task1601;
+
+/* 
+My first thread
+Создать public static class TestThread — нить с интерфейсом Runnable.
+TestThread должен выводить в консоль «My first thread«.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        TestThread task = new TestThread();
+        new Thread(task).start();
+    }
+}
