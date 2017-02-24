c0dd0d9c88fd97d36ddcd52af389fcd5beecf92b
Alik72 <sadvik19@gmail.com>
2017-02-24 10:27:40 +0600

    А без interrupt слабо?
    task16.task1619;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task16.task1619;
+
+/* 
+А без interrupt слабо?
+Разберись, как работает программа.
+Сделай так, чтобы в методе ourInterruptMethod можно было сделать так, чтобы нить TestThread завершилась сама.
+Нельзя использовать метод interrupt.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new TestThread());
+        t.start();
+        Thread.sleep(3000);
+        ourInterruptMethod();
+    }
+
+    public static void ourInterruptMethod() {
+
+        Thread thread = new Thread();
+        thread.run();
+
+    }
+
+    public static class TestThread implements Runnable {
+        public void run() {
+            while (true) {
+                try {
+                    System.out.println("he-he");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
