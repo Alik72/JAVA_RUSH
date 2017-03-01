42ed8e2aacfebdfe6f1700228b4ecf36e1500773
Alik72 <sadvik19@gmail.com>
2017-03-01 13:23:50 +0600

    Общий список
    task17.task1713;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task17/task1713/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task17.task1713;
+
+import java.util.*;
+
+/* 
+Общий список
+1. Изменить класс Solution так, чтобы он стал списком. (Необходимо реализовать интерфейс java.util.List).
+2. Список Solution должен работать только с целыми числами Long.
+3. Воспользуйтесь полем original.
+4. Список будет использоваться нитями, поэтому позаботьтесь, чтобы все методы были синхронизированы.
+*/
+
+public class Solution {
+    private ArrayList<Long> original = new ArrayList<Long>();
+
+    public static void main(String[] args) {
+
+    }
+}
