4de64ecbb56724faa1151332b5723d313c06fc6e
Alik72 <sadvik19@gmail.com>
2017-03-02 11:02:13 +0600

    УРОК 18. Максимальный байт
    task18.task1801;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task18.task1801;
+
+import java.io.FileInputStream;
+
+/* 
+Максимальный байт
+Ввести с консоли имя файла.
+Найти максимальный байт в файле, вывести его на экран.
+Закрыть поток ввода-вывода.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+}
