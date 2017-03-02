4ec91f4383c61107c222c51e4f655fff17612b79
Alik72 <sadvik19@gmail.com>
2017-03-02 11:13:04 +0600

    Минимальный байт
    task.task18.task1802;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1802/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task18.task1802;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* 
+Минимальный байт
+Ввести с консоли имя файла.
+Найти минимальный байт в файле, вывести его на экран.
+Закрыть поток ввода-вывода.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        
+
+    }
+}
