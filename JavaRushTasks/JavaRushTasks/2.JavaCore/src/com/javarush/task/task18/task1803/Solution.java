00fb3022c6f1d98f422d6258439b8a49e89f89c1
Alik72 <sadvik19@gmail.com>
2017-03-02 15:53:51 +0600

    Самые частые байты
    task18.task1803;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1803/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task18.task1803;
+
+import java.io.FileInputStream;
+
+/* 
+Самые частые байты
+Ввести с консоли имя файла.
+Найти байт или байты с максимальным количеством повторов.
+Вывести их на экран через пробел.
+Закрыть поток ввода-вывода.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+}
