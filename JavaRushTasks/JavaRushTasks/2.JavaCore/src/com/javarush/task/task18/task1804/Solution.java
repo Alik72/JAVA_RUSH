2fe6555443f30af0f5018cce5d1bcb33de3dd5f2
Alik72 <sadvik19@gmail.com>
2017-03-03 12:02:40 +0600

    Самые редкие байты
    task18.task1804;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task18.task1804;
+
+import java.io.FileInputStream;
+
+/* 
+Самые редкие байты
+Ввести с консоли имя файла.
+Найти байт или байты с минимальным количеством повторов.
+Вывести их на экран через пробел.
+Закрыть поток ввода-вывода.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+}
