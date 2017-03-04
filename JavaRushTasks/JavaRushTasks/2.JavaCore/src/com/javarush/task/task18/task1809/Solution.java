dd496237c87e9d9702721ed2a3e04359c18f1b55
Alik72 <sadvik19@gmail.com>
2017-03-04 16:10:49 +0600

    Реверс файла
    task18.task1809;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1809/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task18.task1809;
+
+/* 
+Реверс файла
+Считать с консоли 2 имени файла: файл1, файл2.
+Записать в файл2 все байты из файл1, но в обратном порядке.
+Закрыть потоки.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
