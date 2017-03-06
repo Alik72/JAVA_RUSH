3c238b9c9efb820a74df647ad429d39d400d7863
Alik72 <sadvik19@gmail.com>
2017-03-06 14:42:50 +0600

    AmigoOutputStream
    task18.task1813

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task18.task1813;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/* 
+AmigoOutputStream
+1 Измени класс AmigoOutputStream так, чтобы он стал Wrapper-ом для класса FileOutputStream. Используй наследование.
+2 При вызове метода close() должны выполняться следующая последовательность действий:
+2.1 Вызвать метод flush().
+2.2 Дописать следующий текст «JavaRush © All rights reserved.«, используй метод getBytes().
+2.3 Закрыть поток методом close().
+*/
+
+public class AmigoOutputStream {
+    public static String fileName = "C:/tmp/result.txt";
+
+    public static void main(String[] args) throws FileNotFoundException {
+        new AmigoOutputStream(new FileOutputStream(fileName));
+    }
+
+}
