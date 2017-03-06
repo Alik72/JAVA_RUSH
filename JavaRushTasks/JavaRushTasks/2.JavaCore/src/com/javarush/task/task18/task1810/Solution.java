1fa090c9ebcc3053c94067e2852584d15a5f3a03
Alik72 <sadvik19@gmail.com>
2017-03-06 10:56:43 +0600

    DownloadException
    task18.task1810;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task18.task1810;
+
+/* 
+DownloadException
+1 Считывать с консоли имена файлов.
+2 Если файл меньше 1000 байт, то:
+2.1 Закрыть потоки работы с файлами.
+2.2 Выбросить исключение DownloadException.
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws DownloadException, IOException {
+        
+        }
+
+    public static class DownloadException extends Exception {
+
+    }
+}
