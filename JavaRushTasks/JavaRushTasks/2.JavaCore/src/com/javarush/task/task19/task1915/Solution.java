f30d00a3536fe380b8a1ca1543bafaf462a5263f
Alik72 <sadvik19@gmail.com>
2017-03-16 16:20:58 +0600

    Дублируем текст
    task19.task1915;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1915/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task19.task1915;
+
+/* 
+Дублируем текст
+Считай с консоли имя файла.
+В методе main подмени объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна дублировать вывод всего текста в файл, имя которого ты считал.
+Вызови готовый метод printSomething(), воспользуйся testString.
+Верни переменной System.out первоначальный поток.
+Закрой поток файла.
+
+Пример вывода на экран:
+it's a text for testing
+
+Пример тела файла:
+it's a text for testing
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+
