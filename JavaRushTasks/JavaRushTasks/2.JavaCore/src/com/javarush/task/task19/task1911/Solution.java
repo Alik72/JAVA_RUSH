888542975cf8b417e1c372b02110ac97789f03c1
Alik72 <sadvik19@gmail.com>
2017-03-16 12:55:26 +0600

    Ридер обертка
    task19.task1911;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1911/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task19.task1911;
+
+/* 
+Ридер обертка
+В методе main подмени объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна преобразовывать весь текст в заглавные буквы.
+Вызови готовый метод printSomething(), воспользуйся testString.
+Верни переменной System.out первоначальный поток.
+Выведи модифицированную строку в консоль.
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
