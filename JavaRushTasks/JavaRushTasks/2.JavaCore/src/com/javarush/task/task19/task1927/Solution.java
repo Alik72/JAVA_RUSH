2d8e55d0cfce2699cba1e0fff7fcb388f14d3ed6
Alik72 <sadvik19@gmail.com>
2017-03-17 18:17:33 +0600

    Контекстная реклама
    task19.task1927;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task19.task1927;
+
+/* 
+Контекстная реклама
+В методе main подмени объект System.out написанной тобой реадер-оберткой.
+Твоя реадер-обертка должна выводить на консоль контекстную рекламу после каждого второго println-а.
+Вызови готовый метод printSomething(), воспользуйся testString.
+Верни переменной System.out первоначальный поток.
+
+Рекламный текст: «JavaRush — курсы Java онлайн»
+
+Пример вывода:
+first
+second
+JavaRush - курсы Java онлайн
+third
+fourth
+JavaRush - курсы Java онлайн
+fifth
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
