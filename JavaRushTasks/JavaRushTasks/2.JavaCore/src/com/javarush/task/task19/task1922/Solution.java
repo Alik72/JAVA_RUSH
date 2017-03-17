0832c126d54d4679e3a7f2c5a57d63609f1d98fc
Alik72 <sadvik19@gmail.com>
2017-03-17 18:12:54 +0600

    Ищем нужные строки
    task19.task1922;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task19.task1922;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Ищем нужные строки
+Считать с консоли имя файла.
+Вывести в консоль все строки из файла, которые содержат всего 2 слова из списка words.
+Закрыть потоки.
+
+Пример:
+words содержит слова А, Б, В
+
+Строки:
+В Б А Д //3 слова из words, не подходит
+Д А Д //1 слово из words, не подходит
+Д А Б Д //2 слова — подходит, выводим
+*/
+
+public class Solution {
+    public static List<String> words = new ArrayList<String>();
+
+    static {
+        words.add("файл");
+        words.add("вид");
+        words.add("В");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
