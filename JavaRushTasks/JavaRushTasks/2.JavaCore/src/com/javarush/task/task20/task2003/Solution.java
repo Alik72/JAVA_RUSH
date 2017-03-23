d188e41d47bc2c35873eba262a6604060802366d
Alik72 <sadvik19@gmail.com>
2017-03-23 17:31:46 +0600

    Знакомство с properties
    task20.task2003;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task20.task2003;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Знакомство с properties
+В методе fillInPropertiesMap считайте имя файла с консоли и заполни карту properties данными из файла.
+Про .properties почитать тут — http://ru.wikipedia.org/wiki/.properties
+Реализуй логику записи в файл и чтения из файла для карты properties.
+*/
+public class Solution {
+    public static Map<String, String> properties = new HashMap<>();
+
+    public void fillInPropertiesMap() {
+        //implement this method - реализуйте этот метод
+    }
+
+    public void save(OutputStream outputStream) throws Exception {
+        //implement this method - реализуйте этот метод
+    }
+
+    public void load(InputStream inputStream) throws Exception {
+        //implement this method - реализуйте этот метод
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
