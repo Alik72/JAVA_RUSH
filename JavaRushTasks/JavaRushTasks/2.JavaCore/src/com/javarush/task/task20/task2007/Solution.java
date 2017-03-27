d03ab1ea6d70eb125ed1547d92a986b5962b2239
Alik72 <sadvik19@gmail.com>
2017-03-27 16:32:47 +0600

    Как сериализовать JavaRush?
    task20.task2007;

---
+++ JavaRushTasks/JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2007/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task20.task2007;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Как сериализовать JavaRush?
+Сделай так, чтобы сериализация класса JavaRush была возможной.
+*/
+public class Solution {
+    public static class JavaRush {
+        public List<User> users = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
