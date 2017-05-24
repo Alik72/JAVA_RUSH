e662ba6479fdafc8135368a1d317c4016ef54e47
Alik72 <sadvik19@gmail.com>
2017-05-24 11:26:39 +0600

    Ипподром(5)
    task21.task2113.big05
    часть 5

--- JavaRushTasks/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ JavaRushTasks/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -4,4 +4,8 @@ package com.javarush.task.task21.task2113;
  * Created by DDS_TS_2 on 23.05.2017.
  */
 public class Horse {
+    public String name;
+    public double speed;
+    public double distance;
+
 }
