4e8f7a7a1a462f28180781ed7bd3c7166baac18c
Alik72 <sadvik19@gmail.com>
2017-05-24 10:51:48 +0600

    Ипподром(3)
    task21.task2113.big03
    3 часть

--- JavaRushTasks/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ JavaRushTasks/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -6,7 +6,11 @@ import java.util.List;
  * Created by DDS_TS_2 on 23.05.2017.
  */
 public class Hippodrome {
-    
+
+    private List<Horse> horses;
+    public List<Horse> getHorses() {
+        return horses;
+    }
 
     public static void main(String[] args) {
 
