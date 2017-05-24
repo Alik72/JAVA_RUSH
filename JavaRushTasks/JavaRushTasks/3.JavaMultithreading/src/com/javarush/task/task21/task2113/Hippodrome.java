03bdf32c1187725802f2bc309305eb9302342e28
Alik72 <sadvik19@gmail.com>
2017-05-24 10:48:45 +0600

    Ипподром(2)
    task21.task2113.big02
    второй раздел к большой задачи

---
+++ JavaRushTasks/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task21.task2113;
+
+import java.util.List;
+
+/**
+ * Created by DDS_TS_2 on 23.05.2017.
+ */
+public class Hippodrome {
+    
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ JavaRushTasks/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task21.task2113;
+
+/**
+ * Created by DDS_TS_2 on 23.05.2017.
+ */
+public class Horse {
+}
