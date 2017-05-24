3bfd897434a63f7858800972c9d06476b39add01
Alik72 <sadvik19@gmail.com>
2017-05-24 12:57:31 +0600

    Ипподром(7)
    task21.task2113.big07
    часть 7

--- JavaRushTasks/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ JavaRushTasks/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,8 +19,18 @@ public class Hippodrome {
         this.horses = horses;
     }
 
+    public static Hippodrome game;
 
     public static void main(String[] args) {
+
+      game = new Hippodrome(new ArrayList<>()) ;
+
+        Horse o = new Horse("one",3, 0);
+        Horse d = new Horse("two",3, 0);
+        Horse t = new Horse("three",3, 0);
+        game.getHorses().add(o);
+        game.getHorses().add(d);
+        game.getHorses().add(t);
 
     }
 }
