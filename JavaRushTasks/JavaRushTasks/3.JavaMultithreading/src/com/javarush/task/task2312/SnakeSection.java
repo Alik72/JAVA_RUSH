ac91411d117b342749917f43a14dca12ded3f832
Alik72 <sadvik19@gmail.com>
2017-11-16 16:54:37 +0600

    Змейка(8)
    task2312.big08

--- JavaRushTasks/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task2312/SnakeSection.java
+++ JavaRushTasks/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task2312/SnakeSection.java
@@ -4,4 +4,20 @@ package com.javarush.task.task23.task2312;
  * Created by DDS_TS_2 on 12.09.2017.
  */
 public class SnakeSection {
+    private int x;
+    private int y;
+
+    public SnakeSection(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
